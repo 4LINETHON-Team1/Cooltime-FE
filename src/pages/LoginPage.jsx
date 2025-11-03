291ac4a3bf3a218cc42fc38d4f3a7abb3505ed6e
seeun <tpdms3139@naver.com>
2025-11-04 01:54:07 +0900

    Feat/#28/로그인 페이지 수정

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,13 +2,16 @@ import { useState, useEffect } from 'react'
 import Button from '@/components/shared/Button'
 import Back from '@/assets/Back.svg?react'
 import LoginInput from '@/components/signup/LoginInput'
+import { useNavigate } from 'react-router-dom'
 
 const LoginPage = () => {
   const [disabled, setDisabled] = useState(false)
-  const [idNotice, setIdNotice] = useState('default')
-  const [passNotice, setPassNotice] = useState('default')
+  const [idNotice, setIdNotice] = useState('none')
+  const [passNotice, setPassNotice] = useState('none')
   const [id, setId] = useState('')
   const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     if (id.trim() !== '' && password.trim() !== '') {
@@ -18,12 +21,16 @@ const LoginPage = () => {
     }
   }, [id, password])
 
+  const handleWelcome = () => {
+    navigate('/welcome')
+  }
+
   return (
     <div>
       <div className='mt-10 ml-4'>
         <Back />
       </div>
-      <div className='mt-9 ml-4'>
+      <div className='mt-9 ml-5'>
         <h3 className='Title-01-1_2 text-main-500'>로그인을 해주세요.</h3>
       </div>
       <div className='mt-8 flex flex-col items-center'>
@@ -41,7 +48,7 @@ const LoginPage = () => {
         />
       </div>
       <div className='fixed bottom-10 left-1/2 -translate-x-1/2 text-center'>
-        <Button label='다음' onClick={() => {}} disabled={disabled}></Button>
+        <Button label='다음' onClick={handleWelcome} disabled={disabled}></Button>
       </div>
     </div>
   )
