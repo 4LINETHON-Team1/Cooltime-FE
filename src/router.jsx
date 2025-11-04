82a9342607cac6aa596942a4d148eded6dc72c6d
yexnsxo <yslove829@naver.com>
2025-11-04 18:27:32 +0900

    Feat/#33/테스트 유형 질문 리스트 작성

--- src/router.jsx
+++ src/router.jsx
@@ -1,14 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
 import MainPage from './pages/MainPage'
+import TestPage from '@/pages/TestPage'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
-      { index: true, element: <MainPage /> },
+      { index: true, element: <TestPage /> },
       { path: 'main', element: <MainPage /> },
+      { path: 'test', element: <TestPage /> },
     ],
   },
 ])
