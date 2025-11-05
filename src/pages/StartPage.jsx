ebcc2cc213f8d48cf7dfcbb92f8d6840e4c6ded1
yexnsxo <yslove829@naver.com>
2025-11-05 19:31:01 +0900

    Feat/#35/시작화면 애니메이션 적용

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import StartMix from '@/assets/StartMix.svg'
+import { useState, useEffect } from 'react'
+import BgImg1 from '@/assets/BgImg1.svg?react'
+import BgImg2 from '@/assets/BgImg2.svg?react'
+import BgImg3 from '@/assets/BgImg3.svg?react'
 import Button from '@/components/shared/Button'
 
 const StartPage = () => {
+  const [position1, setPosition1] = useState(0)
+  const [position2, setPosition2] = useState(0)
+
+  useEffect(() => {
+    setPosition1(-10)
+    setPosition2(17)
+    const interval = setInterval(() => {
+      setPosition1((prev) => (prev === 0 ? -10 : 0))
+      setPosition2((prev) => (prev === 0 ? 17 : 0))
+    }, 2000)
+    return () => clearInterval(interval)
+  }, [])
   return (
-    <div className='flex flex-col min-h-screen justify-start items-center w-full max-w-[490px] mx-auto bg-linear-to-b from-main-300 via-white to-white'>
+    <div className='flex flex-col min-h-screen justify-start items-center w-full max-w-[440px] mx-auto bg-linear-to-b from-main-300 via-white to-white'>
       <div className='flex flex-col justify-baseline items-start text-left w-full h-full ml-10 mt-[76px]'>
         <div className='mb-8'>
           <h1 className='Title-01-1_1 mb-[-5px]'>미루는 나도,</h1>
@@ -16,11 +30,22 @@ const StartPage = () => {
           <h3 className='body-01-1_2 text-grey-900'>내일의 나를 위한 쿨타임이니까요.</h3>
         </div>
       </div>
-      <img
-        src={StartMix}
-        alt='icon'
-        className='fixed mt-15 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full'
-      />
+      <div className='fixed top-3/5 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full max-w-[440px]'>
+        <div className='relative w-full h-full transition-transform duration-700 ease-in-out'>
+          <BgImg1
+            style={{ transform: `translateY(${position1}px)` }}
+            className='absolute top-1/2 left-34/100 transform -translate-x-1/2 -translate-y-1/2 z-10 transition-transform duration-2000 ease-in-out'
+          />
+          <BgImg2
+            style={{ transform: `translateY(${position2}px)` }}
+            className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-transform duration-2000 ease-in-out'
+          />
+          <BgImg3
+            style={{ transform: `translateY(${position1}px)` }}
+            className='absolute top-1/2 left-5/8 transform -translate-x-1/2 -translate-y-1/2 z-11 transition-transform duration-2000 ease-in-out'
+          />
+        </div>
+      </div>
       <div className='fixed bottom-10 justify-center items-center text-center'>
         <Button label='처음 시작해요' />
         <h3 className='mt-[13px] body-01-1_2 text-gray-400'>이미 계정이 있습니다</h3>
