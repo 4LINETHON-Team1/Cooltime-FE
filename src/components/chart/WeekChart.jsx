5b43539471f8cbb747aa310d9783241b7211c685
seeun <tpdms3139@naver.com>
2025-11-06 00:59:14 +0900

    Feat/#42/주 차트 페이지

---
+++ src/components/chart/WeekChart.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import PieChart from '../report/PieChart'
+import Left from '@/assets/Left.svg?react'
+import Right from '@/assets/Right.svg?react'
+import { getCurrentDate } from '@/utils/dateUtils'
+
+const WeekChart = () => {
+  const { month, week } = getCurrentDate()
+
+  const weekNames = ['첫째주', '둘째주', '셋째주', '넷째주', '다섯째주']
+
+  const initialWeekIndex = weekNames.indexOf(week)
+  const [currentWeek, setCurrentWeek] = useState(initialWeekIndex)
+  const [currentMonth, setCurrentMonth] = useState(month)
+
+  const handlePrevWeek = () => {
+    if (currentWeek > 0) {
+      setCurrentWeek(currentWeek - 1)
+    } else {
+      setCurrentMonth((prev) => (prev === 1 ? 12 : prev - 1))
+      setCurrentWeek(4)
+    }
+  }
+
+  const handleNextWeek = () => {
+    if (currentWeek < 4) {
+      setCurrentWeek(currentWeek + 1)
+    } else {
+      setCurrentMonth((prev) => (prev === 12 ? 1 : prev + 1))
+      setCurrentWeek(0)
+    }
+  }
+
+  const value = 50
+  return (
+    <div className='flex flex-col items-center w-full'>
+      <div className='flex flex-col items-center text-center w-[350px]'>
+        <div className='flex'>
+          <button className='mr-2' onClick={handlePrevWeek}>
+            <Left />
+          </button>
+          <p className='Title-01-1_1 text-black-400'>
+            {currentMonth}월 {weekNames[currentWeek]} 미룸 비율
+          </p>
+          <button className='ml-2 ' onClick={handleNextWeek}>
+            <Right />
+          </button>
+        </div>
+        <p className='body-02-1_2 text-gray-900 mt-4'>저번 주보다 미룸 비율이 00% 상승했어요.</p>
+      </div>
+      <div className='fixed top-65 flex flex-col items-center mt-10 w-[274px] h-[274px]'>
+        <PieChart value={value} width={20} />
+        <div className='absolute flex flex-col items-center mt-25'>
+          <p className='Title-01-1_1 text-black-400'>{value}%</p>
+          <p className='Title-03-3_3 text-gray-400 mt-1'>총 미룸 비율</p>
+        </div>
+      </div>
+      <div className='fixed bottom-30 flex w-[343px] h-[106px] bg-white shadow-xs rounded-2xl mt-10 items-center justify-center'>
+        <div className='flex flex-col items-center mr-16'>
+          <p className='body-02-1_2 text-black-400 mb-1'>했어요</p>
+          <p className='Title-01-1_1 text-black-400'>30</p>
+        </div>
+        <div className='w-px h-[46px] bg-gray-400'></div>
+        <div className='flex flex-col items-center ml-16'>
+          <p className='body-02-1_2 text-black-400 mb-1'>미뤘어요</p>
+          <p className='Title-01-1_1 text-black-400'>30</p>
+        </div>
+        <div className='flex flex-col items-center'></div>
+      </div>
+    </div>
+  )
+}
+
+export default WeekChart
