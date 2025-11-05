273743ba9f7cea96ed946a2af2674196eb53737d
seeun <tpdms3139@naver.com>
2025-11-05 21:47:15 +0900

    Feat/#38/그래프 컴포넌트 추가

---
+++ src/components/report/PieChart.jsx
@@ -0,0 +1,35 @@
+import { PieChart as Chart } from 'react-minimal-pie-chart'
+import { useUserStore } from '@/store/store'
+
+const PieChart = ({ value }) => {
+  const { theme } = useUserStore()
+
+  const color = {
+    blue: '#C8CEFF',
+    mint: '#AEE1D1',
+    peach: '#FFD6B3',
+  }
+  const chartColor = color[theme] ?? '#C8CEFF'
+
+  return (
+    <Chart
+      data={[
+        {
+          value: 100,
+          color: chartColor,
+          name: 'name1',
+        },
+      ]}
+      reveal={value}
+      lineWidth={30}
+      background='#F0F0F0'
+      lengthAngle={360}
+      rounded
+      animate
+      startAngle={-90}
+      center={[90, 50]}
+    />
+  )
+}
+
+export default PieChart
