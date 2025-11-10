7cc1f1b9c6be303dd6a233b2b46bb243b146c6f9
yexnsxo <yslove829@naver.com>
2025-11-10 19:19:03 +0900

    Delete/#69/달력 조회용 더미데이터 삭제

--- src/store/calendarStore.js
+++ src/store/calendarStore.js
@@ -76,48 +76,12 @@ export const useReasonStore = create((set) => ({
   clearSelected: () => set({ selected: new Set() }),
 }))
 
-// 더미 데이터
-const dummyLogs = [
-  {
-    id: 1,
-    user_id: 1,
-    date: '2025-11-01',
-    isPostponed: true,
-    note: null,
-    type: 'Perfectionist',
-  },
-  {
-    id: 2,
-    user_id: 1,
-    date: '2025-11-03',
-    isPostponed: false,
-    note: '공부 미룸',
-    type: 'Low-Motivation',
-  },
-  {
-    id: 3,
-    user_id: 1,
-    date: '2025-11-05',
-    isPostponed: true,
-    note: '운동 못함',
-    type: 'Stress-Prone',
-  },
-  {
-    id: 3,
-    user_id: 1,
-    date: '2025-10-10',
-    isPostponed: true,
-    note: '운동 못함',
-    type: 'Stress-Prone',
-  },
-]
-
 export const useCalendarStore = create((set) => ({
   currentMonth: new Date(),
   selectedDate: null,
-  completedCount: null,
-  postponedCount: null,
-  logs: dummyLogs,
+  completedCount: 0,
+  postponedCount: 0,
+  logs: [],
   setCurrentMonth: (month) => set({ currentMonth: month }),
   setSelectedDate: (date) => set({ selectedDate: date }),
   setLogs: (logs) => set({ logs }),
